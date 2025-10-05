620b98076d2d14442fba3208401f46ec6517726b
blacksheepthe2nd <blacksheepthe2nd@gmail.com>
2025-10-05 09:33:02 -0700

    fix: add initial migrations for main app

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
-from django.db import models
-
-# Create your models here.
+from django.db import models 
+ 
+class DummyModel(models.Model): 
+    name = models.CharField(max_length=100, default="dummy") 
+    created_at = models.DateTimeField(auto_now_add=True) 
+ 
+    def __str__(self): 
+        return self.name 
